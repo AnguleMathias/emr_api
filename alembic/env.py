d0f44b4bf7da8061a282fe23356bfd82b10468d0
AnguleMathias <angulemathias3@gmail.com>
2020-04-11 23:24:45 +0300

    set up migrations paths to models and run migrations offline

--- alembic/env.py
+++ alembic/env.py
@@ -24,3 +24,32 @@ database_url = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 sys.path.append(os.getcwd())
 config.set_main_option('sqlalchemy.url', database_url)
+
+from helpers.database import Base
+from api.user.models import User
+from api.role.models import Role
+
+
+target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+    Calls to context.execute() here emit the given string to the
+    script output.
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
