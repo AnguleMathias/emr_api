b34bdbd405686e6c43ce5bd40b55d72171f0e9a2
AnguleMathias <angulemathias3@gmail.com>
2020-04-12 16:01:13 +0300

    docker configurations 2

--- manage.py
+++ manage.py
@@ -15,9 +15,7 @@ def make_shell_context():
     return dict(app=app)
 
 
-manager.add_command(
-    "shell", Shell(
-        make_context=make_shell_context))
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
