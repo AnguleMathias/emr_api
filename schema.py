ffaddfaf4d5123550da0251732f56fb85344756c
AnguleMathias <angulemathias3@gmail.com>
2020-04-13 02:42:57 +0300

    notifications model

--- schema.py
+++ schema.py
@@ -2,11 +2,13 @@ import graphene
 import api.role.schema
 import api.user.schema
 import api.user.schema_query
+import api.notification.schema
 
 
 class Query(
     api.role.schema.Query,
-    api.user.schema_query.Query
+    api.user.schema_query.Query,
+    api.notification.schema.Query,
 ):
     """Root for emr Graphql queries"""
     pass
@@ -14,7 +16,8 @@ class Query(
 
 class Mutation(
     api.role.schema.Mutation,
-    api.user.schema.Mutation
+    api.user.schema.Mutation,
+    api.notification.schema.Mutation,
 ):
     """Root for graphql mutations"""
     pass
